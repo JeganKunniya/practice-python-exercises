68118095ce6c5608f1f99833016d47d5335b19a7
Jegan Kunniya <jegan.kunniya@gmail.com>
2021-06-19 17:13:24 +0530

    Initial commit of the Python exercises from https://www.practicepython.org/ along with .gitignore.

---
+++ 01-CharacterInput.py
@@ -0,0 +1,15 @@
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+from datetime import date
+
+
+def calculate_when_user_turns_100():
+    user_name = input('Enter your name : ')
+    age = int(input('Enter your age : '))  # age is an integer and not a decimal. Hence the typecast
+
+    print('Hello {}, you will be 100 years by {}'.format(user_name, date.today().year + 100 - age))
+
+
+if __name__ == '__main__':
+    calculate_when_user_turns_100()
---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a main Python script to launch the python exercises.
+# The exercises are from https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
